3f1b28e5fa25a2c78e8a2f207cd65d3e82924072
Max <maximilian.gelbrecht@posteo.de>
2022-12-12 20:49:40 +0100

    fixed tests and readme

--- test/zero-d-embs.jl
+++ test/zero-d-embs.jl
@@ -11,23 +11,23 @@ end
 
 @testset "Boltzmann Model Fixed Points" begin 
     model = ZeroDEBM(R_in=IncomingRadiation(α=TanhAlbedo()), R_out=StefanBoltzmannRadiation()) 
-    prob = ODEProblem(model, 220., (0.,4.), [])
+    prob = ODEProblem(model, [220.], (0.,4.), [])
 
     sol_low = solve(prob, Tsit5())
     @test 230. < sol_low[end][1] < 238.
 
-    sol_high = solve(remake(prob, u0=320), Tsit5())
+    sol_high = solve(remake(prob, u0=[320]), Tsit5())
     @test 286. < sol_high[end][1] < 295.
 end 
 
 @testset "Budyko Model Fixed Points" begin 
     model = ZeroDEBM(R_in=IncomingRadiation(α=TanhAlbedo()), R_out=BudykoRadiation()) 
-    prob = ODEProblem(model, 220., (0.,4.), [])
+    prob = ODEProblem(model, [220.], (0.,4.), [])
 
     sol_low = solve(prob, Tsit5())
     @test 200. < sol_low[end][1] < 208.
 
-    sol_high = solve(remake(prob, u0=320), Tsit5())
+    sol_high = solve(remake(prob, u0=[320]), Tsit5())
     @test 295. < sol_high[end][1] < 305.
 end 
 
